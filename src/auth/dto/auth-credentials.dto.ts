1dd1fda7e34e62277e587ba49d96aa67e218ca40
vitcool@ukr.net <vitcool@ukr.net>
2022-05-10 21:57:37 +0300

    TM-5: validation rules applied to signup properties

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/^(?=.{6,20}$)\D*\d/, { message: 'assword too weak' })
   password: string;
 }
