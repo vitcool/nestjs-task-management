69dfc69751386dc52bf27d294899224bfdf6706e
vitcool@ukr.net <vitcool@ukr.net>
2022-05-31 22:12:56 +0300

    TM9: deployment

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,18 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({
+      origin: serverConfig.origin,
+    });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
