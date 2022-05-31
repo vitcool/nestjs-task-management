9bafa6b21a97b63f584079821528972eb90174f0
vitcool@ukr.net <vitcool@ukr.net>
2022-05-31 23:07:05 +0300

    TM9: typeorm updates

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,8 +8,11 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.PASSWORD || dbConfig.password,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
